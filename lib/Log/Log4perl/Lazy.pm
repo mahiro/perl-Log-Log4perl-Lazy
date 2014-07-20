36431af2af12151fcd39bb472cdefc6a940bf39f
Mahiro Ando <ma514y@gmail.com>
2014-07-20 14:19:01 +0900

    add github links

--- lib/Log/Log4perl/Lazy.pm
+++ lib/Log/Log4perl/Lazy.pm
@@ -133,7 +133,11 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker (report bugs here)
+=item * GitHub repository (report bugs here)
+
+L<https://github.com/mahiro/perl-Log-Log4perl-Lazy>
+
+=item * RT: CPAN's request tracker (report bugs here, alternatively)
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Log4perl-Lazy>
 
